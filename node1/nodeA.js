d58601811fb70b881ab5235b0be4526c077829a0
sheller12 <hikakun21@gmail.com>
2021-03-17 13:45:12 +0900

    add new file

---
+++ node1/nodeA.js
@@ -0,0 +1,6 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+response.writeHead(200, {'Content-Type': 'text/plain'});
+response.end('Hello World!\n');
+}).listen(PORT);
