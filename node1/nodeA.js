9338f85553cf8f22b7c5d3992d7bcd8b5060ac0b
sheller12 <hikakun21@gmail.com>
2021-03-18 12:55:48 +0900

    delete

--- node1/nodeA.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-
-http.createServer((request, response) => {
-response.writeHead(200, {'Content-Type': 'text/plain'});
-response.end('Hello World!\n');
-}).listen(PORT);
